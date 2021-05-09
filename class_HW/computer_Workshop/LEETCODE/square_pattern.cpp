470918d8934b4c056fb721c30e91d63d7e9a24ce
Anurag Yadav <anuragyadavmnp@gmail.com>
2021-05-09 17:39:19 +0530

    added square pattern

---
+++ class_HW/computer_Workshop/LEETCODE/square_pattern.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+
+    for (int i=0; i<2 * num-1; i++) 
+    {
+        int m;
+        if (i<num)
+        {
+            m=i;
+        }
+        
+        else
+        {
+            m=2*(num-1)-i;
+        }
+
+        for (int j=0; j<m; j++)
+            cout << num-j;
+
+        for (int j=0; j<2 * (num-m)-1; j++)
+            cout << num-m;
+
+        for (int j=m-1; j>=0; j--)  
+            cout << num - j;
+
+        cout << endl;
+    }
+    return 0;
+}
