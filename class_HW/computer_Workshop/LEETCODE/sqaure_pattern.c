96b1425c5ac4f44871a0fec5dde923a4529477f2
Anurag Yadav <anuragyadavmnp@gmail.com>
2021-05-09 17:52:59 +0530

    added square pattern in c language

---
+++ class_HW/computer_Workshop/LEETCODE/sqaure_pattern.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+
+    int m=0;
+    for (int i=0; i<2 * num-1; i++) 
+    {
+        if (i<num)
+        {
+            m=i;
+        }
+        
+        else
+        {
+            m=2*(num-1)-i;
+        }
+
+        for (int j=0; j<m; j++)
+            printf("%d", num-j);
+
+        for (int j=0; j<2 * (num-m)-1; j++)
+            printf("%d", num-m);
+
+        for (int j=m-1; j>=0; j--)  
+            printf("%d", num-j);
+
+        printf("\n");
+    }
+    return 0;
+}
