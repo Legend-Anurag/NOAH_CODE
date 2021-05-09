42dfd49124204e3f79b52cea5b1c4b036d3868b5
Anurag Yadav <anuragyadavmnp@gmail.com>
2021-05-09 18:35:54 +0530

    added 1d array sum program

---
+++ class_HW/computer_Workshop/LEETCODE/1d_arr_sum.c
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number/problem
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int size, mod=0, i, ans=0;
+    scanf("%d", &size);
+
+    int inp[size];
+    for (int i=0; i<size; i++)
+    {
+        scanf("%d", &inp[i]);
+    }
+    
+    for (int i=0; i<size; i++)
+    {
+        ans+=inp[i];
+    }
+    printf("%d",ans);
+    
+    return 0;
+}
